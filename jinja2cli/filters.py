c7f2b53b9f25b1b63a8d3271b036b8b6fbf3db06
bckr <bri@bckr.io>
2018-09-02 10:50:24 -0500

    Fix python 3 base64 filtering

--- jinja2cli/filters.py
+++ jinja2cli/filters.py
@@ -5,13 +5,22 @@ Custom jinja filters that extend the Jinja2 dictionary of filters.
 
 License: BSD, see LICENSE for more details.
 """
+from sys import version_info
 from base64 import b64encode, b64decode
 
+PY3 = version_info[0] == 3
+
 def do_b64_encode(value):
-  return b64encode(value)
+  if PY3:
+    return b64encode(value.encode()).decode()
+  else:
+    return b64encode(value)
 
 def do_b64_decode(value):
-  return b64decode(value)
+  if PY3:
+    return b64decode(value.encode()).decode()
+  else:
+    return b64decode(value)
 
 FILTERS = {
   'b64encode':          do_b64_encode,
