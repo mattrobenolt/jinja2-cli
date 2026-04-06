568e963175749375f3effbb6d432378721b531a8
Matt Robenolt <m@robenolt.com>
2026-04-06 13:38:52 -0700

    modernize flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     mattware = {
       url = "github:mattrobenolt/nixpkgs";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -9,36 +9,43 @@
   };
 
   outputs =
-    {
+    inputs@{
+      flake-parts,
       nixpkgs,
-      flake-utils,
       mattware,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ mattware.overlays.default ];
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        { system, ... }:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ mattware.overlays.default ];
+          };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              just
+              hyperfine
+              (bats.withLibraries (p: [
+                p.bats-support
+                p.bats-assert
+                p.bats-file
+              ]))
+              uv
+              shellcheck
+              uvShellHook
+              hadolint
+            ];
+          };
         };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            just
-            hyperfine
-            (bats.withLibraries (p: [
-              p.bats-support
-              p.bats-assert
-              p.bats-file
-            ]))
-            uv
-            shellcheck
-            uvShellHook
-            hadolint
-          ];
-        };
-      }
-    );
+    };
 }
